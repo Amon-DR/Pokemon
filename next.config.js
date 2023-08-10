26d427913faab37b373ada913177e719941b9fc0
Dashaun Riggins <105265788+Amon-DR@users.noreply.github.com>
2023-08-10 12:17:21 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = {
-    ...nextConfig,
-  reactStrictMode: true,
-  images: {
-    domains: ['raw.githubusercontent.com'],    
-    formats: ['image/avif', 'image/webp'],
-  },
-
-}
