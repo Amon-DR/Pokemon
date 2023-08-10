06cec193bdaff928318aea9a7d50498dc16622e6
Dashaun <dashaunriggins@gmail.com>
2023-08-10 11:48:46 -0500

    fixed eslint issue

--- components/pokey/pokey.tsx
+++ components/pokey/pokey.tsx
@@ -78,7 +78,7 @@ export default function Pokey() {
                         randomPokemon
                         :
                         data !== "error" ?
-                            data?.map(() => {
+                            data?.map((data:any) => {
                                 count++
                                 return (
                                     <div className="api-pokemon pause" id={"api-poke-" + (count)}>
