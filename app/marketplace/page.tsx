a9d209c09ac9f8a2e354dc9210700ff9776540ce
Dashaun <dashaunriggins@gmail.com>
2023-08-14 15:03:51 -0500

    error -1

--- app/marketplace/page.tsx
+++
@@ -1,9 +0,0 @@
-import InfoSection from "@/components/infoSection/InfoSection"
-export default function Marketplace(){
-    return(
-        <section>
-            <InfoSection/>
-            <h1>Marketplace</h1>
-        </section>
-    )
-}
